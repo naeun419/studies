1c6d99a3215ca3ff7bc7d9c698948e7014c4dcae
Naeun <naeun0739@gmail.com>
2021-01-21 14:15:07 +0900

    컴포넌트 속성 예제

--- react/src/index.js
+++ react/src/index.js
@@ -2,12 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import App from './App';
 
-var h1 = React.createElement('h1', null, 'Hello World!!!!!')
+let h1 = React.createElement('h1', null, 'Hello World!@?')
+
+class HelloWorld extends React.Component {
+  render() {
+    return React.createElement('h1', this.props, 'Hello ' + this.props.frameworkName + ' World!!!')
+  }
+}
 
 ReactDOM.render(
 
-  h1,
-  document.getElementById('content')
+    React.createElement(
+      'div',
+      null,
+      React.createElement(HelloWorld, {
+        id: 'ember',
+        frameworkName: 'Ember.js',
+        title: 'A framework for creating ambitious web application.'}),
+      React.createElement(HelloWorld, {
+        id: 'backbone',
+        frameworkName: 'Backbone.js',
+        title: 'Backbone.js gives structure to web application...'}),
+      React.createElement(HelloWorld, {
+        id: 'angular',
+        frameworkName: 'Angular.js',
+        title: 'Superheroic JavaScript MVW Framework'})
+    ),
+    document.getElementById('content')
+
+
   // <React.StrictMode>
   //   <App />
   // </React.StrictMode>,
