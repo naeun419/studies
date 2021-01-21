33ea4b410523e272f7fcbea05f5c859944b549af
Naeun <naeun0739@gmail.com>
2021-01-21 10:29:05 +0900

    hello word / README_explanation

--- react/src/index.js
+++ react/src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+// import App from './App';
+
+var h1 = React.createElement('h1', null, 'Hello World!!!!!')
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+
+  h1,
+  document.getElementById('content')
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>,
+  // document.getElementById('root')
 );
