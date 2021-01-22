7948e0f840d5e295578da73ea8819551c44e602f
Naeun <naeun0739@gmail.com>
2021-01-22 11:11:34 +0900

    ch03_JSX / README update

--- react/src/App.js
+++ react/src/App.js
@@ -1,9 +1,17 @@
+import React, { Fragment } from 'react'
+
+import Chapter1 from './chapter1'
+import Chapter2 from './chapter2'
+import Chapter3 from './chapter3'
+
 function App() {
   return (
-    <div>
-      <p>Hello World!</p>
-    </div>
-  );
+    <>
+      <Chapter1 />
+      <Chapter2 />
+      <Chapter3 />
+    </>
+  )
 }
 
 export default App;
