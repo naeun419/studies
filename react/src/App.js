a6a033f363615137054fde61a64b9a8a6333fa76
Naeun <naeun0739@gmail.com>
2021-01-27 09:31:26 +0900

    ch04_ReactComponentState, ch05_ReactComponentLifecycle / README update

--- react/src/App.js
+++ react/src/App.js
@@ -3,13 +3,17 @@ import React, { Fragment } from 'react'
 import Chapter1 from './chapter1'
 import Chapter2 from './chapter2'
 import Chapter3 from './chapter3'
+import Chapter4 from './chapter4'
+import Chapter5 from './chapter5'
 
 function App() {
   return (
     <>
-      <Chapter1 />
+      {/* <Chapter1 />
       <Chapter2 />
       <Chapter3 />
+      <Chapter4 /> */}
+      <Chapter5 />
     </>
   )
 }
--- react/src/index.js
+++ react/src/index.js
@@ -2,50 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-//let h1 = React.createElement('h1', null, 'Hello World!@?')
-//let helloWorldReactElement = <h1>Hello World!?!</h1>
-
-// class HelloWorld extends React.Component {
-//   render() {
-//     return (
-//       <div>
-//         <h1>1. hello world! </h1>
-//         <h1>2. hello world! </h1>
-//       </div>
-//     )
-//     //return React.createElement('h1', this.props, 'Hello ' + this.props.frameworkName + ' World!!!')
-//   }
-// }
-
-
 ReactDOM.render(
 
-  //helloWorldReactElement,
-  // <HelloWorld/>,
   <App />,
   document.getElementById('content')
 
-    // React.createElement(
-    //   'div',
-    //   null,
-    //   React.createElement(HelloWorld, {
-    //     id: 'ember',
-    //     frameworkName: 'Ember.js',
-    //     title: 'A framework for creating ambitious web application.'}),
-    //   React.createElement(HelloWorld, {
-    //     id: 'backbone',
-    //     frameworkName: 'Backbone.js',
-    //     title: 'Backbone.js gives structure to web application...'}),
-    //   React.createElement(HelloWorld, {
-    //     id: 'angular',
-    //     frameworkName: 'Angular.js',
-    //     title: 'Superheroic JavaScript MVW Framework'})
-    // ),
-    // document.getElementById('content')
-
-
-  // <React.StrictMode>
-  //   <App />
-  // </React.StrictMode>,
-  // document.getElementById('root')
 );
